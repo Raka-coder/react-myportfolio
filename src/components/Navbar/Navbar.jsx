f90694c4b3293be9a3babeaaf9e5094a40d58d52
Raka <incudelta@gmail.com>
2025-02-26 21:40:57 +0700

    edit: navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,12 +12,6 @@ function Navbar() {
             </Link>
           </div>
           <div className="flex flex-row gap-6 mt-6 md:mt-0 md:ml-auto items-center">
-            {/* <Link
-              to="/blog"
-              className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative"
-            >
-              Blog
-            </Link> */}
             <Link
               to="/project"
               className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative"
@@ -30,7 +24,6 @@ function Navbar() {
             >
               Photos
             </Link>
-            
           </div>
         </div>
       </nav>
