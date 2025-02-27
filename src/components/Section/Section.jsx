c5f4a65cf6d27f0d95d0d5c100832a817410e704
Raka <incudelta@gmail.com>
2025-02-27 12:28:51 +0700

    refactor: remove unnecessary Link component from Section

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function Section() {
   const currentYear = new Date().getFullYear();
@@ -8,17 +7,15 @@ function Section() {
       <main className="flex-auto min-w-0 md:mt-6 flex flex-col px-6 sm:px-4 md:px-0 max-w-[630px] w-full font-atkinson text-zinc-100">
         <div className="flex flex-wrap justify-center md:justify-start lg:justify-start xl:justify-start">
           <div className="w-full md:w-1/2 lg:w-2/3 xl:w-3/4">
-            <Link to="/" target="_blank" rel="noopener noreferrer">
-              <img
-                src="https://turdif.vercel.app/assets/raka-31P2Tfho.jpg"
-                width={120}
-                height={120}
-                className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 sm:float-right sm:ml-5 sm:mb-5 grayscale-25 hover:grayscale-0 -mr-36 transition duration-150"
-                alt=""
-                loading="lazy"
-                title="Raka Restu Saputra"
-              />
-            </Link>
+            <img
+              src="https://turdif.vercel.app/assets/raka-31P2Tfho.jpg"
+              width={120}
+              height={120}
+              className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 sm:float-right sm:ml-5 sm:mb-5 grayscale-25 hover:grayscale-0 -mr-36 transition duration-150"
+              alt=""
+              loading="lazy"
+              title="Raka Restu Saputra"
+            />
             <h1 className="text-3xl font-bold mb-6">Welcome to my profile!</h1>
             <p className="text-lg mb-6 text-zinc-100">
               Hi! my name is Raka Restu Saputra. I am {currentYear - 2005}'s
@@ -31,7 +28,8 @@ function Section() {
             networking, hardware, and web development.
           </p>
           <p className="text-lg mb-6 text-zinc-100">
-            If you want to know more about me, you can visit my social media or contact me for more information.
+            If you want to know more about me, you can visit my social media or
+            contact me for more information.
           </p>
         </div>
       </main>
